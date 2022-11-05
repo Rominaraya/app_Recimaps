232d5aea6624609b247a0d02d5f46b71de19060d
Rominaraya <rarayaa@utem.cl>
2022-11-05 16:56:05 -0300

    change register and login

--- app/src/main/java/com/recimaps/recimaps/LoginActivity.kt
+++ app/src/main/java/com/recimaps/recimaps/LoginActivity.kt
@@ -1,11 +1,59 @@
 package com.recimaps.recimaps
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.google.firebase.auth.FirebaseAuth
+import com.recimaps.recimaps.databinding.ActivityLoginBinding
+
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        val screenSplash = installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        screenSplash.setKeepOnScreenCondition { false }
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.btnCreateAccount.setOnClickListener {
+            val intent = Intent( this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnStar.setOnClickListener {
+            val email = binding.etEmail.text.toString()
+            val pass = binding.etPassword.text.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty()){
+
+                firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
+                    if (it.isSuccessful){
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else{
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }else{
+                Toast.makeText( this , "No se permiten campos vacíos" , Toast.LENGTH_SHORT).show()
+            }
+        }
     }
+     //mantener sesion inciada
+    /*override fun onStart() {
+        super.onStart()
+
+        if (firebaseAuth.currentUser != null){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }*/
 }
--- app/src/main/java/com/recimaps/recimaps/RegisterActivity.kt
+++ app/src/main/java/com/recimaps/recimaps/RegisterActivity.kt
@@ -1,11 +1,52 @@
 package com.recimaps.recimaps
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.recimaps.recimaps.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.tvRegisterReady.setOnClickListener {
+            val intent = Intent( this , LoginActivity::class.java)
+            startActivity(intent)
+        }
+        binding.btnSend.setOnClickListener {
+            val email = binding.etEmailAddress.text.toString()
+            val pass = binding.etPassword.text.toString()
+            val confirmPass = binding.etConfirmation.text.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()){
+                if (pass == confirmPass){
+
+                    firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener{
+                        if (it.isSuccessful){
+                            val intent = Intent( this , LoginActivity::class.java)
+                            startActivity(intent)
+
+                        }else{
+                            Toast.makeText( this , it.exception.toString() , Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }else{
+                    Toast.makeText( this , "La contraseña no coincide" , Toast.LENGTH_SHORT).show()
+                }
+            }else{
+                Toast.makeText( this , "No se permiten campos vacíos" , Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
